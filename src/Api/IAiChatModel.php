176ea4708685fab3890a90f0267d45f23208e49b
Daniel Dahme <danieldahme@googlemail.com>
2026-03-27 20:28:53 +0000

    header added

--- src/Api/IAiChatModel.php
+++ src/Api/IAiChatModel.php
@@ -1,5 +1,20 @@
 <?php declare(strict_types=1);
 
+/***********************************************************************
+ * This file is part of AssistantFoundation for BASE3 Framework.
+ *
+ * AssistantFoundation extends the BASE3 framework with a unified API
+ * foundation for assistants, chatbots, and agent-based systems.
+ * It provides shared interfaces for modular AI integration.
+ *
+ * Developed by Daniel Dahme
+ * Licensed under GPL-3.0
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * https://base3.de/v/assistantfoundation
+ * https://github.com/ddbase3/AssistantFoundation
+ **********************************************************************/
+
 namespace AssistantFoundation\Api;
 
 interface IAiChatModel {
